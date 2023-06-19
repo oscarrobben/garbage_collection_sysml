82750d911fd1f2001f6c3fcf9fd62ae3fcd82334
Musicalprogramming <oscarrobben98@gmail.com>
2023-06-19 17:30:23 +0200

    some changes'

---
+++ DefaultComponent/TopLevel_Simulation/MainDefaultComponent.cpp
@@ -0,0 +1,31 @@
+/********************************************************************
+	Rhapsody	: 9.0 
+	Login		: 20172539
+	Component	: DefaultComponent 
+	Configuration 	: TopLevel_Simulation
+	Model Element	: TopLevel_Simulation
+//!	Generated Date	: Mon, 19, Jun 2023  
+	File Path	: DefaultComponent\TopLevel_Simulation\MainDefaultComponent.cpp
+*********************************************************************/
+
+//## auto_generated
+#include "MainDefaultComponent.h"
+int main(int argc, char* argv[]) {
+    int status = 0;
+    if(OXF::initialize(argc, argv, 6423))
+        {
+            //#[ configuration DefaultComponent::TopLevel_Simulation 
+            //#]
+            OXF::start();
+            status = 0;
+        }
+    else
+        {
+            status = 1;
+        }
+    return status;
+}
+
+/*********************************************************************
+	File Path	: DefaultComponent\TopLevel_Simulation\MainDefaultComponent.cpp
+*********************************************************************/
---
+++ DefaultComponent/TopLevel_Simulation/UseCaseAnalysisPkg.h
@@ -0,0 +1,27 @@
+/*********************************************************************
+	Rhapsody	: 9.0 
+	Login		: 20172539
+	Component	: DefaultComponent 
+	Configuration 	: TopLevel_Simulation
+	Model Element	: UseCaseAnalysisPkg
+//!	Generated Date	: Mon, 19, Jun 2023  
+	File Path	: DefaultComponent\TopLevel_Simulation\UseCaseAnalysisPkg.h
+*********************************************************************/
+
+#ifndef UseCaseAnalysisPkg_H
+#define UseCaseAnalysisPkg_H
+
+//## auto_generated
+#include <oxf\oxf.h>
+//## auto_generated
+#include <..\Profiles\SysML\SIDefinitions.h>
+//## auto_generated
+#include <aom\aom.h>
+//## package UseCaseAnalysisPkg
+
+
+
+#endif
+/*********************************************************************
+	File Path	: DefaultComponent\TopLevel_Simulation\UseCaseAnalysisPkg.h
+*********************************************************************/
